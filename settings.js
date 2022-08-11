ab6914e67f7d0f01dba3a5da45144fe1f43625d2
furyv2 <111026904+furyv2@users.noreply.github.com>
2022-08-11 15:54:24 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -21,18 +21,18 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['916909137213']
-global.premium = ['916909137213']
-global.ownernomer = '916909137213'
-global.ownername = '🦄Dream Guy Xeon'
-global.botname = 'Cheems Bot MD V2'
-global.footer = 'Xeon Bot Inc.'
-global.ig = 'https://github.com/DGXeon'
+global.owner = ['919755062790']
+global.premium = ['919755062790']
+global.ownernomer = '919755062790'
+global.ownername = 'nick fury'
+global.botname = 'JOKERV2'
+global.footer = 'nikki.'
+global.ig = '....'
 global.region = 'India, NorthEast, Mizoram'
-global.sc = 'https://github.com/DGXeon/CheemsBot-MD2'
-global.myweb = 'https://youtu.be/imFIX-Wrt3s'
-global.packname = 'Cheems Bot'
-global.author = '🦄Dream Guy Xeon'
+global.sc = '....'
+global.myweb = '....'
+global.packname = '!nik!'
+global.author = '🦄nick fury'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
